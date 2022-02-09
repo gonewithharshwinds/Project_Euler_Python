6c51de82ddf4d4cfb1f7b328165c6d6ac7742203
Harsh Mehta <harsh.mehta2001@gmail.com>
2022-02-09 05:43:00 +0530

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# PROJECT EULER IN PYTHON
+
+### Problems solved :
+
+
+* [p001.py : Multiples of 3 and 5](https://projecteuler.net/problem=1)
+
---
+++ p002.py
@@ -0,0 +1,17 @@
+# ================================================================
+# Project Euler Problem 2
+# ================================================================
+# Copyright © gonewithharshwinds @ 2022. All rights reserved.
+# ================================================================
+# https://github.com/gonewithharshwinds/Project-Euler-Python
+# ================================================================
+def solve():
+    s,a,b = 0,1,2
+    while a <= 4000000:
+        if a % 2 == 0:
+            s += a
+        a, b = b, a + b
+    return str(s)
+
+if __name__ == "__main__":
+    print(solve())
