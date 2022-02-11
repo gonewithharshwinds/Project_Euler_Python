34405a9120131dafa1ecbdd1295b110d0ce15a1b
Harsh ADV) <70069185+gonewithharshwinds@users.noreply.github.com>
2022-02-11 07:10:27 +0530

    Multiples of 3 and 5

---
+++ p001.py
@@ -0,0 +1,14 @@
+# ================================================================
+# Project Euler Problem 1 : Multiples of 3 and 5
+# ================================================================
+# Copyright © gonewithharshwinds @ 2022. All rights reserved.
+# ================================================================
+# https://github.com/gonewithharshwinds/Project-Euler-Python
+# ================================================================
+
+def solve():
+	s = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
+	return str(s)
+
+if __name__ == "__main__":
+	print(solve())
